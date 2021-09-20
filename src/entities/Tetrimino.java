11022f86d96e3b6e44382d842968622f887ec4af
Federico Arias <ariasfede596@gmail.com>
2021-09-20 08:50:29 -0300

    Modificaciones a Tetrimino

--- src/entities/Tetrimino.java
+++ src/entities/Tetrimino.java
@@ -5,52 +5,83 @@ import java.util.ArrayList;
 public abstract class Tetrimino {
 	//Atributos de clase
 	protected String pathPhoto; 
-	protected int rotacion;
-	protected Celda X;
-	protected Celda Y;
-	protected Celda Z;
-	protected Celda W; 
+	protected int rotacion; 
 	protected ArrayList<Celda> listaCeldas; 
 	
 	public Tetrimino(int rotacion, String pathPhoto, Celda c1, Celda c2, Celda c3, Celda c4)  {
 		this.rotacion = rotacion;
 		this.pathPhoto = pathPhoto;
-		this.X = c1;
-		this.Y = c2;
-		this.Z = c3;
-		this.W = c4;
-		listaCeldas.add(X);
-		listaCeldas.add(Y);
-		listaCeldas.add(Z);
-		listaCeldas.add(W);
+		listaCeldas.add(c1);
+		listaCeldas.add(c2);
+		listaCeldas.add(c3);
+		listaCeldas.add(c4);
 	}
 	
-	abstract public PairTupla[] rotar(); 
+	abstract public ArrayList<PairTupla> rotar(); 
 	
 	public ArrayList<PairTupla> moverDerecha() {
-		boolean encontrado = false;
-		int p = 3;
 		//Esto se puede optimizar probablemente
 		ArrayList<PairTupla> retorno = new ArrayList<PairTupla>();
 		//Esto se puede optimizar probablemente
-		retorno.add(new PairTupla(X.getX(), X.getY())); 
-		retorno.add(new PairTupla(Y.getX(), Y.getY())); 
-		retorno.add(new PairTupla(Z.getX(), Z.getY())); 
-		retorno.add(new PairTupla(W.getX(), W.getY()));
-		
-		//Buscamos solo aquellas posiciones (x,y) que serán modificadas al mover a la derecha el tetrimino 
-		for(int i =0; i<=p; i++) {
-			PairTupla auxPair = retorno.get(i);
-			for(int j = 0; j<=3 && !encontrado; j++) {
-				Celda auxCelda = listaCeldas.get(j);
-				if(auxCelda.getX() == auxPair.getX() && auxCelda.getY() == auxPair.getY()) {
-					encontrado=true;
-					p--;
-					retorno.remove(i);
-				}
-			}
-			encontrado=false;
+		for(int i = 0; i<=3; i++) {
+			Celda aux = listaCeldas.get(i); 
+			retorno.add(new PairTupla(aux.getX()+1, aux.getY()));		
 		}
+		
+		posicionesAModificar(retorno);
 		return retorno;
 	}
+	
+	public ArrayList<PairTupla> moverIzquierda(){
+		//Esto se puede optimizar probablemente
+				ArrayList<PairTupla> retorno = new ArrayList<PairTupla>();
+				//Esto se puede optimizar probablemente
+		for(int i = 0; i<=3; i++) {
+			Celda aux = listaCeldas.get(i); 
+			retorno.add(new PairTupla(aux.getX()-1, aux.getY()));		
+		}			
+			posicionesAModificar(retorno);
+			return retorno;
+	}
+	
+	public String getPathPhoto() {
+		return pathPhoto;
+	}
+	
+	public ArrayList<PairTupla> moverAbajo(){
+		//Esto se puede optimizar probablemente
+				ArrayList<PairTupla> retorno = new ArrayList<PairTupla>();
+				//Esto se puede optimizar probablemente
+		for(int i = 0; i<=3; i++) {
+			Celda aux = listaCeldas.get(i); 
+			retorno.add(new PairTupla(aux.getX(), aux.getY()+1));		
+		}			
+			posicionesAModificar(retorno);
+			return retorno;
+	}
+	
+	//A modificar
+	public ArrayList<Celda> getCeldas(){
+		return listaCeldas;
+	}
+	
+	//Modifica una estructura, en este caso ArrayList, dejando solo aquellas posiciones futuras que serán modificadas 
+	//debido a alguna modificación de la ocupación del tetrimino.
+	private void posicionesAModificar(ArrayList<PairTupla> lista ){
+		boolean encontrado = false;
+		int p = 3;
+		for(int i =0; i<=p; i++) {
+					PairTupla auxPair = lista.get(i);
+					for(int j = 0; j<=3 && !encontrado; j++) {
+						Celda auxCelda = listaCeldas.get(j);
+						if(auxCelda.getX() == auxPair.getX() && auxCelda.getY() == auxPair.getY()) {
+							encontrado=true;
+							p--;
+							lista.remove(i);
+						}
+					}
+					encontrado=false;
+				}
+	}
+	
 }
